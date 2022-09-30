a0970c1b4c4c20d4e2d38cfcbdf08ed4fb8c3b7c
rakitenkogermans <rakitenko.germans@gmail.com>
2022-09-30 21:15:25 +0300

    eslint and prettier on the backend

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 100,
+    proseWrap: 'always',
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    semi: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+};
