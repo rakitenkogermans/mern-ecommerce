87382e06d8a1b3b4b57fc5495ba607c5f4aece8c
rakitenkogermans <rakitenko.germans@gmail.com>
2022-09-30 20:59:28 +0300

    morgan write to file

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,20 @@ import { userRouter } from "./routes/userRoutes";
 import { orderRouter } from "./routes/orderRoutes";
 import { uploadRouter } from "./routes/uploadRoutes";
 import path from "path";
+import * as fs from "fs";
 
 config({ path: `.env.${process.env.NODE_ENV}` });
 
 const PORT = process.env.PORT || 5000;
 const app: Express = express();
+
+app.use(
+  morgan("common", {
+    stream: fs.createWriteStream(path.resolve(__dirname, "access.log"), {
+      flags: "a",
+    }),
+  })
+);
 
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
